20345914779c18baae82fe9924365e01356e7547
coreajinho <coreajinho1@naver.com>
2025-12-15 01:40:16 +0900

    reafactor/ 팀원찾기 게시판 레이아웃 변경

--- src/pages/findTeam/index.jsx
+++ src/pages/findTeam/index.jsx
@@ -55,8 +55,8 @@ function FindTeamPage() {
   return (
     <div className="bg-gray-50 min-h-screen">
       <div className="max-w-[1500px] mx-auto px-4 py-6">
-        {/* 2-Column Grid Layout (왼쪽 필터, 중앙 게시글, 오른쪽 비어있음) */}
-        <div className="grid grid-cols-1 lg:grid-cols-[280px_1fr_280px] xl:grid-cols-[320px_1fr_320px] gap-6 items-start">
+        {/* 2-Column Grid Layout (왼쪽 필터, 오른쪽 게시글) */}
+        <div className="grid grid-cols-1 lg:grid-cols-[280px_1fr] xl:grid-cols-[320px_1fr] gap-6 items-start">
           {/* 왼쪽: 필터 영역 (모바일에서는 숨김) */}
           <div className="hidden lg:block">
             <FilterSection 
@@ -70,7 +70,7 @@ function FindTeamPage() {
             />
           </div>
           
-          {/* 가운데: 팀원찾기 게시글 목록 또는 내 요청 목록 */}
+          {/* 오른쪽: 팀원찾기 게시글 목록 또는 내 요청 목록 */}
           {showMyRequests ? (
             <RequestList />
           ) : (
@@ -81,11 +81,6 @@ function FindTeamPage() {
               isFilterLoading={isFilterLoading}
             />
           )}
-          
-          {/* 오른쪽: 빈 공간 (향후 확장 가능) */}
-          <div className="hidden lg:block">
-            {/* 빈 공간 */}
-          </div>
         </div>
       </div>
     </div>
